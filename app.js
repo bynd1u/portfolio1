6d1d2a060e9ba432e4c797bdcce7487208fb3224
bynd1u <semenbindu@gmail.com>
2025-01-28 16:36:32 +0100

    highlight current section

--- app.js
+++ app.js
@@ -8,3 +8,28 @@ function openMenu() {
 }
 
 hamburger.addEventListener("click", openMenu);
+
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a, nav ul li a");
+
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (
+      scrollY >= sectionTop - 100 &&
+      scrollY < sectionTop + sectionHeight - 100
+    ) {
+      currentSection = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${currentSection}`) {
+      link.classList.add("active");
+    }
+  });
+});
